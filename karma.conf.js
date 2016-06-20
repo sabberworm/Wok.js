1d97451782849bf095e9fd74879dc55f772e0a70
Raphael Schweikert <any@sabberworm.com>
2016-06-20 11:03:35 +0200

    Try replacing PhantomJS with Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -44,6 +44,12 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
